067068bf937b6458d651153371bc705f6ba1667e
Michel Eduardo <90931413+MichelEduardoBZ@users.noreply.github.com>
2023-07-30 06:39:34 -0300

    Cart moch data

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,32 +3,40 @@ import SeachBar from "../../../componente/SearchBar";
 import CatalogCard from "../../../componente/CatalogCard";
 import ButtonNextPage from "../../../componente/ButtonNextPage";
 import { ProductDTO } from "../../../models/product";
-import * as productService from '../../../services/product-service'
+import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
-export default function Catalog() {
+import { CategoryDTO } from "../../../models/category";
 
+export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  const objTest: CategoryDTO = {
+    id: 8,
+    name: "Jardinagem",
+  };
+
   useEffect(() => {
-    productService.findAll()
-    .then(response => {
+    // localStorage.setItem("minhaCategoria", JSON.stringify(objTest))
+
+    const obj = JSON.parse(localStorage.getItem("minhaCategoria") || "{}");
+    console.log(obj);
+
+    productService.findAll().then((response) => {
       setProducts(response.data.content);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-      <main>
-        <section id="catalog-section" className="dsc-container">
-            <SeachBar></SeachBar>
-          <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-
-            {
-              products.map(product => <CatalogCard key={product.id} product={product}></CatalogCard>
-              )
-            }
-          </div>
-          <ButtonNextPage text="Carregar Mais"></ButtonNextPage>
-        </section>
-      </main>
+    <main>
+      <section id="catalog-section" className="dsc-container">
+        <SeachBar></SeachBar>
+        <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+          {products.map((product) => (
+            <CatalogCard key={product.id} product={product}></CatalogCard>
+          ))}
+        </div>
+        <ButtonNextPage text="Carregar Mais"></ButtonNextPage>
+      </section>
+    </main>
   );
 }
