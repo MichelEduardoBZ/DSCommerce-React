f14c5fa3861ddcdefcafaa6e6f5c9b9086da7722
Michel Eduardo <90931413+MichelEduardoBZ@users.noreply.github.com>
2023-07-31 08:46:35 -0300

    Catalog next page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,21 +6,38 @@ import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
 
+type QueryParams = {
+  page: number;
+  name: string;
+};
+
 export default function Catalog() {
+  const [isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
-  const [productName, setProductName] = useState("");
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page: 0,
+    name: "",
+  });
 
   useEffect(() => {
+    productService
+      .findPageRequest(queryParams.page, queryParams.name)
+      .then((response) => {
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
+      });
+  }, [queryParams]);
 
-    productService.findPageRequest(0, productName)
-    .then((response) => {
-      setProducts(response.data.content);
-    });
-  }, [productName]);
+  function handleSearch(searchText: string) {
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
 
-  function handleSearch(searchText: string){
-    setProductName(searchText)
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 });
   }
 
   return (
@@ -32,7 +49,12 @@ export default function Catalog() {
             <CatalogCard key={product.id} product={product}></CatalogCard>
           ))}
         </div>
-        <ButtonNextPage text="Carregar Mais"></ButtonNextPage>
+        {
+          !isLastPage &&
+          <div onClick={handleNextPageClick}>
+            <ButtonNextPage text="Carregar Mais"></ButtonNextPage>
+          </div>
+        }
       </section>
     </main>
   );
